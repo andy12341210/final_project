dfd17b02c3648c858bf4f207ff3dc9e94bb1db86
Adeer <andy12341210@gmail.com>
2023-01-05 10:18:57 +0800

    01051018

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,31 @@
 import mongo from "./mongo.js";
 import httpServer from "./server.js";
+import express from "express"
+import cors from "cors"
+import path from "path"
 
 import "dotenv-defaults/config.js";
 
 mongo.connect();
 const port = process.env.PORT | 4000;
+const port2 = process.env.PORT | 4001;
+
+const app = express();
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(cors());
+    app.use(express.json());
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+  }
+
+app.listen(port2, () =>
+ console.log(`app listening on port ${port}!`),
+);
 
 
 httpServer.listen({ port }, () => {
